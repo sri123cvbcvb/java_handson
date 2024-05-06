7a4b755b6105135d93fa0553f968eb64ee77b52e
sri123cvbcvb <sriramjeeva01@gmail.com>
2024-05-06 10:07:29 +0530

    second

---
+++ Set_interface_handson.java
@@ -0,0 +1,20 @@
+public class Set_interface_handson {
+    
+    /* -> set interface ia an one of the part of the collection framework. it represents a generally 
+    unordered with collection of unique values. it will not allow dupicate values in the collection. 
+    the commonly used implementation of the set interface are hashset, treeset, linkedhashset.
+
+    hashset:
+         -> most popular set
+         -> the widely used implementation of the set interface based on the hash table
+         -> the hash table stores data in key - value pair so it provides fast in operations.
+         -> it provides constant time performance for basic oprations like adding, remove, 
+         contains(checking whether the values present in the set interface or not) 
+         -> constant time com
+     */
+
+     public static void main(String[] args) {
+        
+     }
+
+}
