66c98acd4bc3fd2a420e6164d457097648d454b4
sri123cvbcvb <sriramjeeva01@gmail.com>
2024-05-06 10:30:39 +0530

    first

---
+++ StreameExample.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.*;
+
+public class Set_interface_handson {
+    
+    /* -> set interface ia an one of the part of the collection framework. it represents a generally 
+    unordered with collection of unique values. it will not allow dupicate values in the collection. 
+    the commonly used implementation of the set interface are hashset, treeset, linkedhashset.
+
+    hashset:
+         -> most popular set
+         -> the widely used implementation of the set interface based on the hash table
+         -> the hash table stores data in key - value pair so it provides fast in operations.
+         -> it provides constant time performance for basic oprations like adding, remove, 
+         contains(checking whether the values present in the set interface or not) 
+         -> constant time com
+     */
+
+     public static void main(String[] args) {
+        List<Integer> name = Arrays.asList(2,3,4,5);
+         int even = name.stream().filter(n -> n%2==0).reduce(0,(res,i)-> res+i);
+         System.out.println(even);
+     }
+
+}
